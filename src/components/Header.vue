f450160c26bc2cd14f14869cde249757379173ca
andreafontaneto <andrea.fontaneto@gmail.com>
2021-12-01 11:18:18 +0100

    header + main structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
   <header>
-    HEADER
+    
+    <h1>BOOLFLIX</h1>
+
+    <div class="search-box">
+      <input type="text" placeholder="Cerca il film o serie tv...">
+      <button>CERCA</button>
+    </div>
+
+
   </header>
 </template>
 
@@ -11,5 +19,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+header{
+  width: 100vw;
+  height: 80px;
+  background-color: black;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1{
+    color: red;
+  }
+
+  input{
+    width: 300px;
+    height: 30px;
+    margin-right: 20px;
+  }
+
+  button{
+    width: 100px;
+    height: 30px;
+    background-color: deepskyblue;
+    color: white;
+  }
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
-    MAIN
+    
+    <div class="card">
+      <h3 class="title">TITOLO</h3>
+      <p class="original-title">Titolo originale: TITOLO ORIGINALE</p>
+      <p class="lang">Lingua: LINGUA</p>
+      <p class="rate">Voto: VOTO</p>
+    </div>
+
   </main>
 </template>
 
@@ -11,5 +18,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+main{
+  height: calc(100vh - 80px);
+  background-color: rgb(44, 44, 44);
+  padding: 50px;
+
+  .card{
+    width: 250px;
+    height: 400px;
+    background-color: grey;
+    color: white;
+    padding: 20px;
+    
+    .title{
+      text-align: center;
+      padding-bottom: 20px;
+    }
+
+    .original-title,
+    .lang,
+    .rate{
+      font-size: 12px;
+      padding-bottom: 20px;
+    }
+  }
+}
 
 </style>
